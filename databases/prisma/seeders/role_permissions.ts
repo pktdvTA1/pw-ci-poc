a31a37d0d9b0037cf6d668505b01105bfc4b9f45
pipat <pipat.k@terradigitalventures.com>
2024-07-02 14:46:20 +0700

    add seeders and func

--- databases/prisma/seeders/role_permissions.ts
+++ databases/prisma/seeders/role_permissions.ts
@@ -1,12 +1,21 @@
-export const role_permissions = [
-	{
+import { permissions } from './permissions';
+
+const saPermissions = Object.keys(permissions).map((v, i) => {
+	return {
 		role_id: 1,
-		permission_id: 1,
+		permission_id: i + 1,
 		is_active: true,
+	};
+});
+
+export const role_permissions = [
+	...saPermissions,
+	{
+		role_id: 2,
+		permission_id: 5,
 	},
 	{
-		role_id: 1,
-		permission_id: 2,
-		is_active: true,
+		role_id: 2,
+		permission_id: 6,
 	},
 ];
--- databases/prisma/seeders/users.ts
+++ databases/prisma/seeders/users.ts
@@ -1,12 +1,23 @@
 export const users = [
 	{
-		external_id: 'xxx',
-		email: 'pipat.k@terradigitalventures.com',
+		external_id: 'a001',
 		first_name: 'pipat',
 		last_name: 'kae',
+		email: 'pipat.k@email.com',
 		is_active: true,
 		created_by: 'mock@mock.com',
 		updated_by: 'mock@mock.com',
 		role_id: 1,
+	},
+	{
+		external_id: 'b001',
+		email: 'prinada.c@email.com',
+		first_name: 'prinada',
+		last_name: 'co',
+		is_active: true,
+		phone_number: '0832223333',
+		created_by: 'mock@mock.com',
+		updated_by: 'mock@mock.com',
+		role_id: 2,
 	},
 ];
