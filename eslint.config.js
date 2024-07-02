4ef7b54a15d8a4a6c0730667e1079172c0c0937d
pipat <pipat.k@terradigitalventures.com>
2024-07-02 15:14:34 +0700

    update eslint again

--- eslint.config.js
+++ eslint.config.js
@@ -6,30 +6,18 @@ export default [
 	...[eslint.configs.recommended, ...tseslint.configs.recommended].map((c) => ({
 		...c,
 		files: ['**/*.ts'],
-	})),
-	{
-		// override recommended eslint and tseslint here
-		files: ['**/*.ts'],
 		rules: {
 			'@typescript-eslint/no-explicit-any': 'off',
 			'@typescript-eslint/no-namespace': 'off',
+			'@typescript-eslint/no-unused-vars': 'error',
+			'@typescript-eslint/no-inner-declarations': 'off',
 		},
-	},
+	})),
 	{
 		files: ['tests/**/*.spec.ts'],
 		...playwright.configs['flat/recommended'],
-	},
-	{
-		// override playwright eslint () here
-		// can refer to this https://www.npmjs.com/package/eslint-plugin-playwright
-		files: ['tests/**/*.spec.ts', 'tests/**/*.setup.ts', 'tests/**/*.page.ts'],
 		rules: {
-			'@typescript-eslint/no-namespace': 'off',
-			'no-inner-declarations': 'off',
-			'no-unused-vars': 'off',
-			'@typescript-eslint/no-explicit-any': 'off',
-			'@typescript-eslint/no-unused-vars': 'warn',
-			'playwright/no-nested-step': 'off',
+			'playwright/no-nested-step': 'warn',
 			'playwright/prefer-strict-equal': 'error',
 			'playwright/prefer-to-be': 'warn',
 			'playwright/prefer-to-contain': 'error',
--- tests/integration/004-api-db.spec.ts
+++ tests/integration/004-api-db.spec.ts
@@ -2,6 +2,6 @@ import { test } from '@playwright/test';
 
 test.describe('Prisma DB and Setup', () => {
 	test('Test Something', async () => {
-		console.log('something');
+		console.log('something1s');
 	});
 });
