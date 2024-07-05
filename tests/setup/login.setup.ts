7c04352bf28a5c4a14a6e44c6aea8cff8d2a9ec2
pipat <pipat.k@terradigitalventures.com>
2024-07-05 12:33:00 +0700

    fix missing arg

--- tests/setup/login.setup.ts
+++ tests/setup/login.setup.ts
@@ -8,13 +8,13 @@ let inventoryAssertion: InventoryService.InventoryAssertion;
 setup.describe.configure({ mode: 'parallel' });
 
 setup.describe('login setup tests', () => {
-	setup.beforeEach(async ({ page }) => {
+	setup.beforeEach(async ({ page, request }) => {
 		loginPage = new LoginService.LoginPage(page);
-		inventoryAssertion = new InventoryService.InventoryAssertion(page);
+		inventoryAssertion = new InventoryService.InventoryAssertion(page, request);
 		await loginPage.goto();
 	});
 	setup.afterAll(async () => {
-		await loginPage.page.close();
+		loginPage.page.close();
 	});
 
 	setup('login_setup', async ({ page }) => {
